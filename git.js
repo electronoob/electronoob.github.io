4acb32446d1c97d3244ce778850b7562d57e242f
electronoob <github@electronoob.com>
2014-10-02 01:32:03 +0100

    added git.js by Yonaba.

---
+++ git.js
@@ -0,0 +1,38 @@
+/* 
+http://yonaba.github.io/2012/08/14/List-your-GitHub-projects-using-JavaScript-and-jQuery.md.html
+Using code found at Yonaba's blog where it is written:
+
+"I hope that might help. Feel free to use it, without any credits.
+If you come up with a better version, with nice tweaks or improvements, let me know via comments, or contact me! Happy coding!"
+
+*/
+
+
+jQuery.githubUser = function(username, callback) {
+   jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
+}
+ 
+jQuery.fn.loadRepositories = function(username) {
+    this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
+     
+    var target = this;
+    $.githubUser(username, function(data) {
+        var repos = data.data; // JSON Parsing
+        sortByName(repos);    
+     
+        var list = $('<dl/>');
+        target.empty().append(list);
+        $(repos).each(function() {
+            if (this.name != (username.toLowerCase()+'.github.com')) {
+                list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
+                list.append('<dd>' + this.description +'</dd>');
+            }
+        });      
+      });
+      
+    function sortByName(repos) {
+        repos.sort(function(a,b) {
+        return a.name - b.name;
+       });
+    }
+};
