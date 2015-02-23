f192e783bf4ebb46684c8f9c7fa4c583b160d3f9
Electronoob <git@electronoob.com>
2015-02-23 22:23:50 +0000

    lalalala

--- ytvlc/subs.js
+++ ytvlc/subs.js
@@ -35,8 +35,8 @@ function webAddItem (title, id, description, img) {
   html = "\
 <div style='width: 400px; border: 2px solid #c0c0c0; padding: 20px; margin: 20px;'>\
     <img style='width: 30px; height: 30px;' src='"+img+"'/><br/>\
-    <span>"+title+"</span>\
-    <a href='https://www.youtube.com/channel/"+id+"'>"+description+"</a>\
+    <a href='https://www.youtube.com/channel/"+id+"'><span>"+title+"</span></a>\
+    <p>"+description+"</p>\
 </div>\
 ";
   $("#output").append (html);
