ed0f2d8d60d9243fea4f18cfafa1ac797d4cedf2
Electronoob <git@electronoob.com>
2015-02-23 20:46:04 +0000

    fuckit

--- ytvlc/subs.js
+++ ytvlc/subs.js
@@ -5,7 +5,7 @@ function handleAPILoaded() {
 function getsubs() {
   var request = gapi.client.youtube.subscriptions.list({
     mine: true,
-    part: 'contentDetails,subscriberSnippet',
+    part: 'contentDetails,snippet,subscriberSnippet',
     maxResults: 50,
     order: 'alphabetical'
   });
