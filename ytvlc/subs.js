3a8cb677e53c38d26a086700b41eb18882c8a589
Electronoob <git@electronoob.com>
2015-02-26 10:52:43 +0000

    thumbs

--- ytvlc/subs.js
+++ ytvlc/subs.js
@@ -23,7 +23,7 @@ function getsubs(pageToken) {
         nextPageToken = response.result.nextPageToken;
         prevPageToken = response.result.prevPageToken;
         $.each(response.result.items, function (index, item) {
-          webAddItem(item.snippet.title, item.snippet.resourceId.channelId, item.snippet.description, item.snippet.thumbnails.heigh.url);
+          webAddItem(item.snippet.title, item.snippet.resourceId.channelId, item.snippet.description, item.snippet.thumbnails.default.url);
         });
         if (response.result.nextPageToken)
             getsubs(response.result.nextPageToken); 
